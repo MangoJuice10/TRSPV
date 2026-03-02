94cf89558c8e3999a392b94f350b40af70e1c457
MangoJuice10 <swordmaster10A@gmail.com>
2026-03-02 10:20:38 +0300

    Finish lab work 3

---
+++ ТРСПВ ЛР 3/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+#include <format>
+#include "mpi.h"
+using namespace std;
+
+template <typename T, size_t N>
+string arrToStr(T(&arr)[N]) {
+	string result = "";
+	for (int i = 0; i < N; i++) {
+		result += to_string(arr[i]) + " ";
+	}
+	return result;
+}
+
+template <typename T, size_t N>
+void printArr(T(&arr)[N]) {
+	cout << arrToStr(arr) << endl;
+}
+
+template <typename T, size_t R, size_t C>
+string matrixToStr(T(&matrix)[R][C]) {
+	string result = "";
+	for (size_t i = 0; i < R; i++) {
+		for (size_t j = 0; j < C; j++) {
+			result += to_string(matrix[i][j]) + " ";
+		}
+		result += "\n";
+	}
+	return result;
+}
+
+template <typename T, size_t R, size_t C>
+void printMatrix(T(&matrix)[R][C]) {
+	cout << matrixToStr(matrix) << endl;
+}
+
+int main(int argc, char* argv[]) {
+	const int
+		N = 4,
+		PROCS_N = 4;
+
+	MPI_Init(&argc, &argv);
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	MPI_Comm MPI_COMM_RING;
+	int dims[1] = { N };
+	int periods[1] = { 1 };
+	int reorder = 0;
+
+	MPI_Cart_create(MPI_COMM_WORLD, 1, &dims[0], &periods[0], reorder, &MPI_COMM_RING);
+
+	int
+		A[N][N],
+		B[N][N],
+		C[N][N];
+
+	int
+		rowA[N],
+		rowB[N];
+
+	int rowC[N] = { 0 };
+
+	const int offset = 10;
+
+	if (rank == 0) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				A[i][j] = (N * i) + j + offset;
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				B[i][j] = (N * i) + j + N * N + offset;
+			}
+		}
+
+		cout << format("Матрица A: \n{}", matrixToStr(A)) << endl;
+		cout << format("Матрица B: \n{}", matrixToStr(B)) << endl;
+	}
+
+	MPI_Scatter(&A[0], N, MPI_INT, &rowA[0], N, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Scatter(&B[0], N, MPI_INT, &rowB[0], N, MPI_INT, 0, MPI_COMM_WORLD);
+
+	cout << format("Строка матрицы A, полученная процессом {}: \n{}", rank, arrToStr(rowA)) << endl;
+	cout << format("Строка матрицы B, полученная процессом {}: \n{}", rank, arrToStr(rowB)) << endl;
+
+	int ringRank;
+	MPI_Comm_rank(MPI_COMM_RING, &ringRank);
+
+	int left = (ringRank - 1 + PROCS_N) % PROCS_N;
+	int right = (ringRank + 1) % PROCS_N;
+
+	cout << format("Процесс-отправитель для процесса {}: {}", ringRank, left) << endl;
+	cout << format("Процесс-получатель для процесса {}: {}", ringRank, right) << endl;
+
+	for (int iter = 0; iter < N; iter++) {
+		int i = (rank - iter + N) % N;
+		for (int j = 0; j < N; j++) {
+			rowC[j] += rowA[i] * rowB[j];
+		}
+		MPI_Sendrecv_replace(rowB, N, MPI_INT, right, 0, left, 0, MPI_COMM_RING, MPI_STATUS_IGNORE);
+	}
+
+	MPI_Gather(rowC, N, MPI_INT, C, N, MPI_INT, 0, MPI_COMM_RING);
+
+	if (rank == 0) {
+		cout << format("Результирующая матрица C: \n{}", matrixToStr(C)) << endl;
+	}
+
+	MPI_Comm_free(&MPI_COMM_RING);
+	MPI_Finalize();
+	return 0;
+}
